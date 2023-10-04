5a035408c3d9170465ba19d13ce627bf6f89782e
Jackk-Doe <dodo-stv@hotmail.com>
2023-10-04 22:05:00 +0700

    Update logger.go

--- config/logger/logger.go
+++ config/logger/logger.go
@@ -10,13 +10,12 @@ import (
 func SetLoggerMiddleware(app *fiber.App) {
 
 	/// OPTION 1 - Display all logs
-	// My preferrenced Logger config
 	// myLogger := logger.Config{
 	// 	Output: os.Stdout,
 	// 	Format: `[API] ${cyan}${time} |${yellow}${status}${reset}|${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
 	// 		`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | REQUEST_ID: ${locals:requestid} | QUERY_PARAM: ${queryParams} ` +
 	// 		`| REQUEST_BODY: ${cyan}${body} ${reset} | REQUEST_HEADERS: ${cyan}${header:Authorization} ${reset} ` +
-	// 		`| RESPONSE_BODY: ${cyan}${resBody} ${reset} | ERROR ${red}${error}` + "\n",
+	// 		`| RESPONSE_BODY: ${cyan}${resBody} ${reset} | ERROR: ${red}${error}` + "\n",
 	// 	TimeFormat: "2006/01/02 - 15:04:05",
 	// 	// TimeZone:   "Asia/Bangkok",
 	// }
@@ -27,7 +26,7 @@ func SetLoggerMiddleware(app *fiber.App) {
 		Format: `[API] ${cyan}${time} | ${yellow}${status}${reset} | ${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
 			`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | QUERY_PARAM: ${queryParams} | REQUEST_ID: ${locals:requestid} ` +
 			`| REQUEST_BODY: ${cyan}- ${reset} | REQUEST_HEADERS: ${cyan}${header:Authorization} ${reset} ` +
-			`| RESPONSE_BODY: ${cyan}- ${reset} | ERROR ${red}${error}` + "\n",
+			`| RESPONSE_BODY: ${cyan}- ${reset} | ERROR: ${red}${error}` + "\n",
 		TimeFormat: "2006/01/02 - 15:04:05",
 		// TimeZone:   "Asia/Bangkok",
 	}
