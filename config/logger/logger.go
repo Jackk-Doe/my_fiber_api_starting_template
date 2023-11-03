f8cb7e3d6bef0875d31766ae112b3916f93c3fa9
Jackk-Doe <dodo-stv@hotmail.com>
2023-11-03 14:28:24 +0700

    Update: logger format

--- config/logger/logger.go
+++ config/logger/logger.go
@@ -12,8 +12,8 @@ func SetLoggerMiddleware(app *fiber.App) {
 	/// OPTION 1 - Display all logs
 	myLogger := logger.Config{
 		Output: os.Stdout,
-		Format: `[API] ${cyan}${time} |${yellow}${status}${reset}|${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
-			`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | REQUEST_ID: ${locals:requestid} | QUERY_PARAM: ${queryParams} ` +
+		Format: `[API] ${cyan}${time} | REQUEST_ID: ${locals:requestid} | ${yellow}${status}${reset} | ${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
+			`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | QUERY_PARAM: ${queryParams} ` +
 			`| REQUEST_BODY: ${cyan}${body} ${reset} | REQUEST_HEADERS: ${cyan}${header:Authorization} ${reset} ` +
 			`| RESPONSE_BODY: ${cyan}${resBody} ${reset} | ERROR: ${red}${error}` + "\n",
 		TimeFormat: "2006/01/02 - 15:04:05",
@@ -23,8 +23,8 @@ func SetLoggerMiddleware(app *fiber.App) {
 	/// OPTION 2 - Display only necessary logs (not include request body and response body)
 	// myLogger := logger.Config{
 	// 	Output: os.Stdout,
-	// 	Format: `[API] ${cyan}${time} | ${yellow}${status}${reset} | ${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
-	// 		`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | QUERY_PARAM: ${queryParams} | REQUEST_ID: ${locals:requestid} ` +
+	// 	Format: `[API] ${cyan}${time} | REQUEST_ID: ${locals:requestid} | ${yellow}${status}${reset} | ${blue}${method}${reset} | ${yellow}${latency}${reset} ` +
+	// 		`| IP: ${green}${ip}${reset} | PATH: ${magenta}${path}${reset} | QUERY_PARAM: ${queryParams} ` +
 	// 		`| REQUEST_BODY: ${cyan}- ${reset} | REQUEST_HEADERS: ${cyan}${header:Authorization} ${reset} ` +
 	// 		`| RESPONSE_BODY: ${cyan}- ${reset} | ERROR: ${red}${error}` + "\n",
 	// 	TimeFormat: "2006/01/02 - 15:04:05",
