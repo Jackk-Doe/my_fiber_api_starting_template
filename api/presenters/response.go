82d55a2b272ce4edd3823a8b438afa289693b658
Jackk-Doe <dodo-stv@hotmail.com>
2023-12-19 14:21:42 +0700

    Update: api representer list data

--- api/presenters/response.go
+++ api/presenters/response.go
@@ -11,6 +11,9 @@ const (
 	FAIL    = 0
 )
 
+/*
+Return success response
+*/
 func ResponseSuccess(data interface{}) fiber.Map {
 	t := time.Now()
 	return fiber.Map{
@@ -18,5 +21,26 @@ func ResponseSuccess(data interface{}) fiber.Map {
 		"status":    SUCCESS,
 		"items":     data,
 		"error":     nil,
+	}
+}
+
+/*
+Return list data with pagination infos
+*/
+// NOTE: if not pagination infos is not required, pass -1 to currentPage, currentPageTotalItem, totalPage
+func ResponseSuccessListData(data interface{}, currentPage, currentPageTotalItem, totalPage int) fiber.Map {
+	t := time.Now()
+	return fiber.Map{
+		"timestamp": t.Format("2006-01-02-15-04-05"),
+		"status":    SUCCESS,
+		"items": fiber.Map{
+			"list_data": data,
+			"pagination": fiber.Map{
+				"current_page":            currentPage,
+				"current_page_total_item": currentPageTotalItem,
+				"total_page":              totalPage,
+			},
+		},
+		"error": nil,
 	}
 }
